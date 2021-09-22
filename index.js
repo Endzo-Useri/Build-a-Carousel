be21dc12ae62d11c15c7900fa00155e40efc8614
endzo-useri <coffeeblack7@protonmail.com>
2021-09-21 21:44:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const totalSlides = slides.length;
 
 document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
 document.getElementById('carousel-button-next').addEventListener('click', moveToPrevSlide);
+
+function moveToNextSlide() {
+
+}
+
+function moveToPrevSlide() {
+	
+}
